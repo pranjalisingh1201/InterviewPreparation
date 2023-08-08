4095f3aa42d1f47179eb2c25cb52e8e5058cc119
pranjalisingh1201 <pranjalisingh1201@gmail.com>
2023-08-09 00:50:28 +0530

    Linked List.

---
+++ Linked List/Basic Linked List/ReverseLinkedlistRec.java
@@ -0,0 +1,61 @@
+// Reverse a linked list in java using recursion.
+/* 
+ * Method 1:
+ * The idea is to reverse the linked list starting from head.next to last node.
+ * 
+ * head -> x1 -> x2 -> x3 -> --------- -> xn
+ * head -> x1 -> x2 <- x3 <- --------- <- xn <- reversed_head
+ *          |
+ *      reversed_tail
+ * Method 2:
+ * The idea is to reverse the linked list starting from head to last-1 node.
+ * 
+ * head -> x1 -> x2 -> x3 -> --------- -> xi-1 -> xi -> xi+1 -> ----- -> xn
+ * null <- x1 <- x2 <- x3 <- --------- <-xi-1 -> xi -> xi+1 -> ----- -> xn
+ *                                         |      |      |
+ *                                        prev   curr    |
+ *                                               prev    curr
+*/
+public class ReverseLinkedlistRec {
+    public static void main(String[] args) {
+        
+        Node head = new Node(10);
+        head.next = new Node(20);
+        head.next.next = new Node(30);
+        head.next.next.next = new Node(40);
+        Node prev = null;
+        head = reverselist2(head,prev);
+        
+        while(head!=null)
+        { 
+            System.out.println(head.data);
+            head = head.next;
+        }
+    }
+
+    public static Node reverselist(Node head)
+    {
+        if(head==null || head.next==null) return head;
+        Node revHead = reverselist(head.next);// recursive call to get the last node
+        Node revTail = head.next;//Intialize 
+        revTail.next = head;
+        head.next = null;
+        return revHead;
+    }
+
+    public static Node reverselist2(Node curr,Node prev)
+    {
+        if(curr==null)return prev;
+        Node next = curr.next;
+        curr.next = prev;
+        return reverselist2(next,curr);  
+    }
+}
+
+class Node {
+    int data;
+    Node next;
+    Node(int data) {
+        this.data = data;
+    }
+}
